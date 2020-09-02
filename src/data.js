124b05394f573e0b6505928da162e57b5a53d13d
Stefan <iuhasstefannorbert@yahoo.com>
2020-09-02 15:39:53 +0300

    add test data

---
+++ src/data.js
@@ -0,0 +1,46 @@
+let i = 0;
+
+module.exports = [
+    {
+        id: i++
+        ,name: "Football"
+        ,price: 49.99
+        ,category: "Sporting Goods"
+        ,inStock: true
+    },
+    {
+        id: i++
+        ,name: "Baseball"
+        ,price: 9.99
+        ,category: "Sporting Goods"
+        ,inStock: true
+    },
+    {
+        id: i++
+        ,name: "Basketball"
+        ,price: 29.99
+        ,category: "Sporting Goods"
+        ,inStock: false
+    },
+    {
+        id: i++
+        ,name: "iPad Touch"
+        ,price: 99.99
+        ,category: "Electronics"
+        ,inStock: true
+    },
+    {
+        id: i++
+        ,name: "iPhone 5"
+        ,price: 399.99
+        ,category: "Electronics"
+        ,inStock: false
+    },
+    {
+        id: i++
+        ,name: "Nexus 7"
+        ,price: 399.99
+        ,category: "Electronics"
+        ,inStock: true
+    }
+];
