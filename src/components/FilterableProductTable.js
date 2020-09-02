09c6c6a9cfead789b175d246c4f12d6b22d3350e
Stefan <iuhasstefannorbert@yahoo.com>
2020-09-02 18:46:57 +0300

    filter products

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -5,13 +5,34 @@ import ProductTable from './ProductTable';
 import SearchBar from './SearchBar';
 
 class FilterableProductTable extends React.Component {
+    onFilterChange = (name, value) => {
+        this.setState(
+            {
+                [name]: value
+            }
+        );
+    };
+
     constructor(props) {
         super(props);
         this.state = {
+            filterText: ''
+            ,isOnlyStock: false
         };
     }
 
     render() {
+        const filterText = this.state.filterText;
+        const isOnlyStock = this.state.isOnlyStock;
+        let filteredProducts = products;
+        if(filterText){
+            filteredProducts = filteredProducts.filter(product => product.name.indexOf(filterText) > -1);
+        }
+        if(isOnlyStock){
+            filteredProducts = filteredProducts.filter(product => product.inStock);
+        }
+
+
         const styles = {
             width: '400px',
             padding: '10px',
@@ -19,8 +40,8 @@ class FilterableProductTable extends React.Component {
         };
         return (
             <div style={styles} className="filterableProductTable">
-                <SearchBar/>
-                <ProductTable products={products}/>
+                <SearchBar onChange={this.onFilterChange} filterText={this.state.filterText} isOnlyStock={this.props.isOnlyStock}/>
+                <ProductTable products={filteredProducts}/>
             </div>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
+
+    onChange = (e) => {
+        const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+        this.props.onChange(e.target.name, value);
+    };
 
     render() {
         return (
             <form>
-                <input type="text" placeholder="Search..."/>
-                <input type="checkbox" id="onlyStock"/>
+                <input type="text" placeholder="Search..." onChange={this.onChange} value={this.props.filterText} name="filterText"/>
+                <input type="checkbox" id="onlyStock" onChange={this.onChange} checked={this.props.isOnlyStock} name="isOnlyStock"/>
                 <label htmlFor="onlyStock">Only show products in stock</label>
             </form>
         );
